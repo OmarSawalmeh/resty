4ece7682e6646252d15bd0195749d309eb96a646
Omar Sawalmeh <sawalmehomar10@gmail.com>
2022-08-14 01:46:26 +0300

    Done

--- resty/src/components/form/index.js
+++ resty/src/components/form/index.js
@@ -1,36 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react'
+import axios from 'axios';
+import './form.scss'
 
-import './form.scss';
+function Form(props) {
+  //const [data, setData] = useState();
+  const [method, setMethod] = useState('GET')
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon')
 
-function Form(props){
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
-    };
-    props.handleApiCall(formData);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // const formData = {
+    //   method: method,
+    //   url: url,
+    //   data: 'tt',
+    // }
+    // props.handleApiCall(formData)
   }
 
- 
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
+  const changeUrl = (e) => {
+    setUrl(e.target.value)
+  }
+
+  const changeMethod = (method) => {
+    setMethod(method)
+  }
+
+  const browseUrl = async(e)=>{
+    if(method==="GET"){
+      const data = await axios.get(url);
+      console.log(data);
+      props.handleApiCall(data);
+    }
+    else if(method==='POST'){
+      const data = await axios.post(url)
+      props.handleApiCall(data);
+    }
+    else if (method === 'DELETE') {
+      const data = await axios.delete(url)
+      props.handleApiCall(data);
+    }
+    else if (method === 'PUT') {
+      const data = await axios.put(url)
+      props.handleApiCall(data);
+    }
+    
+  }
+
+  
+
+  return (
+    <>
+      <div data-testid='method' className='Req'>
+        Request Method : {method}
+      </div>
+      <div className='Url'>URL : {url}</div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>URL: </span>
+          <input name='url' type='text' onChange={changeUrl} />
+          <button type='submit' onClick={browseUrl}>
+            GO!
+          </button>
+        </label>
+        <label className='methods'>
+          <button id='get' onClick={() => changeMethod('GET')}>
+            GET
+          </button>
+          <button
+            data-testid='postButton'
+            id='post'
+            onClick={() => changeMethod('POST')}
+          >
+            POST
+          </button>
+          <button id='put' onClick={() => changeMethod('PUT')}>
+            PUT
+          </button>
+          <button id='delete' onClick={() => changeMethod('DELETE')}>
+            DELETE
+          </button>
+        </label>
+      </form>
+    </>
+  )
 }
 
-export default Form;
+export default Form
