1dbffa7debf389a0ab524372757880ebabd03f88
Omar Sawalmeh <sawalmehomar10@gmail.com>
2022-08-15 17:38:12 +0300

    Modify result component

--- resty/src/components/results/index.js
+++ resty/src/components/results/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import './result.scss'
+import JSONPretty from 'react-json-pretty'
 
 function Results(props) {
   return (
+    // <section>
+    //   <div>
+    //     <h1>Header</h1>
+    //     <pre>
+    //       {props.data ? JSON.stringify(props.data.headers, undefined, 2) : null}
+    //     </pre>
+    //     <h1>Data</h1>
+    //     <pre>
+    //       {props.data ? JSON.stringify(props.data.data, undefined, 2) : null}
+    //     </pre>
+    //   </div>
+    // </section>
+
     <section>
-      <div>
-        <h1>Header</h1>
-        <pre>
-          {props.data ? JSON.stringify(props.data.headers, undefined, 2) : null}
-        </pre>
-        <h1>Data</h1>
-        <pre>
-          {props.data ? JSON.stringify(props.data.data, undefined, 2) : null}
-        </pre>
-      </div>
+      {/* <pre>{props.data ? prettyjson(props.data, false, 2) : null}</pre> */}
+      {/* <JSONPretty id='json-pretty' data={props.data.data} /> */}
+      
+      <JSONPretty id='json-pretty' data={props.data} />
     </section>
   )
 }
